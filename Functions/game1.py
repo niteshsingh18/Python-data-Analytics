eb2f32c565e5e8eab657bff484fa485fea125239
nitesh <ak7niteshsingh@gmail.com>
2023-12-12 13:54:55 +0530

    list_tuple_set

--- Functions/game1.py
+++ Functions/game1.py
@@ -2,26 +2,50 @@ import pgzrun
 
 WIDTH = 500
 HEIGHT = 500
+import random
 
 alien = Actor("alien")
 alien.pos = (400, 50)
-box = Rect((20, 20), (100, 100))
+box = Rect((20, 20), (40, 40))
+score = 0
 
 def draw():
     screen.clear()
-    screen.draw.filled_rect(box, "red")
+    screen.draw.filled_rect(box, "blue")
     alien.draw()
 
 def update():
+    global score
     if keyboard.right:
         alien.x = alien.x + 2
     elif keyboard.left:
         alien.x = alien.x - 2
-    box.x = box.x + 2
-    if box.x > WIDTH:
-        box.x = 0
+    elif keyboard.up:
+        alien.y = alien.y - 2
+    elif keyboard.down:
+        alien.y = alien.y + 2
+    if alien.x > WIDTH:
+        alien.x = 0
+    if alien.x < 0:
+        alien.x = WIDTH
+    if alien.y > HEIGHT:
+        alien.y = 0
+    if alien.y < 0:
+        alien.y = HEIGHT   
     if alien.colliderect(box):
-        print("hit")
+        box.x = random.randint(0, WIDTH)
+        box.y = random.randint(0, HEIGHT)
+        score = score + 1
+        print("Score:", score)
+
+        if alien.x > WIDTH:
+            alien.x = 0
+        if alien.x < 0:
+            alien.x = WIDTH
+        if alien.y > HEIGHT:
+            alien.y = 0
+        if alien.y < 0:
+            alien.y = HEIGHT
 
 
 pgzrun.go()
