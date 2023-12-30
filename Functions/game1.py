3f0e116d41933e86471a24f4de02009a86cc665f
nitesh <ak7niteshsingh@gmail.com>
2023-12-31 01:55:42 +0530

    pandas_#1

--- Functions/game1.py
+++ Functions/game1.py
@@ -8,6 +8,7 @@ alien = Actor("alien")
 alien.pos = (400, 50)
 box = Rect((20, 20), (40, 40))
 score = 0
+speed = 5
 
 def draw():
     screen.clear()
@@ -17,13 +18,13 @@ def draw():
 def update():
     global score
     if keyboard.right:
-        alien.x = alien.x + 2
+        alien.x = alien.x + 10
     elif keyboard.left:
-        alien.x = alien.x - 2
+        alien.x = alien.x - 10
     elif keyboard.up:
-        alien.y = alien.y - 2
+        alien.y = alien.y - 10
     elif keyboard.down:
-        alien.y = alien.y + 2
+        alien.y = alien.y + 10
     if alien.x > WIDTH:
         alien.x = 0
     if alien.x < 0:
